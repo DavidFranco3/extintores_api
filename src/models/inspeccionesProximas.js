19edf4665b0e926046deef2a35e3504d622ea488
David Franco <josedavidayalafranco3@gmail.com>
2025-02-24 14:51:23 -0600

    correcciones

--- src/models/inspeccionesProximas.js
+++ src/models/inspeccionesProximas.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 
 
 // modelo de la coleccion usuarios
-const inspecciones = new Schema({
+const inspeccionesProximas = new Schema({
   idFrecuencia: { type: String },
   estado: { type: String },
 }, {
@@ -13,15 +13,15 @@ const inspecciones = new Schema({
 
 // Middleware `pre` para ajustar las fechas antes de guardar
 // Middleware `pre` para ajustar las fechas antes de devolverlas (si es necesario)
-inspecciones.methods.formatDates = function () {
+inspeccionesProximas.methods.formatDates = function () {
   this.createdAt = moment(this.createdAt).tz('America/Mexico_City').toDate();
   this.updatedAt = moment(this.updatedAt).tz('America/Mexico_City').toDate();
 };
 
 // Middleware `pre` para ajustar las fechas antes de guardarlas (si necesitas hacer alguna transformación al momento de guardar)
-inspecciones.pre('save', function (next) {
+inspeccionesProximas.pre('save', function (next) {
   // Si necesitas hacer ajustes antes de guardar, puedes hacerlo aquí
   next();
 });
 
-module.exports = mongoose.model("inspecciones", inspecciones, "inspecciones");
+module.exports = mongoose.model("inspeccionesProximas", inspeccionesProximas, "inspeccionesProximas");
